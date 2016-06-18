b0ae33277382a4d41ba47e203d5659d736f7c79d
Codespace <codespaceperu@gmail.com>
2016-06-17 23:42:30 -0500

    Update

---
+++ app/src/main/java/pe/com/codespace/cie10/MyValues.java
@@ -0,0 +1,10 @@
+package pe.com.codespace.cie10;
+
+/**
+ * Creado por Carlos on 18/04/2015.
+ */
+public final class MyValues {
+
+    public final static int VOICE_RECOGNITION_REQUEST_CODE = 12345;
+
+}
